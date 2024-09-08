a22d48b3e81942e9dd2e35385802a6d7ba870e2b
sabha348 <sabha341781@gmail.com>
2024-09-08 14:25:45 +0530

    Add MyUserRoute for user creation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import MyUserRoute from './routes/MyUserRoute';
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -10,6 +11,9 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+
+app.use("/api/my/user",MyUserRoute);
 
 app.get("/test", async (req: Request, res: Response) => {
     res.json({ message: "Hello!" });
---
+++ backend/src/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+
+const userSchema= new mongoose.Schema({
+    auth0id:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+    },
+    addressline1: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+}
+)
+const User=mongoose.model("User",userSchema);
+export default User;
